11cbd27c9641d77aba60a7ea5be48560f233fda9
Katrina Pritchard <pritckat@gmail.com>
2018-08-14 19:19:35 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(count)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(count)
+  while count > 0
+    puts "#{count} SECOND(S)!"
+    count -=1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
+end
